a23cfadea909b53d1924e11a4b5a9fbf1ac4640e
Roger Taule <rtaule@polygon.technology>
2025-05-28 10:02:31 +0000

    Adding extern c functions

---
+++ src/setup/circom/build/goldilocks_base_field.cpp.d
@@ -0,0 +1,7 @@
+build/goldilocks_base_field.cpp.o: goldilocks_base_field.cpp \
+ goldilocks_base_field.hpp goldilocks_base_field_tools.hpp \
+ goldilocks_base_field_scalar.hpp goldilocks_base_field_avx.hpp
+goldilocks_base_field.hpp:
+goldilocks_base_field_tools.hpp:
+goldilocks_base_field_scalar.hpp:
+goldilocks_base_field_avx.hpp:
